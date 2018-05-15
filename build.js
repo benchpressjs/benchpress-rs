f7b6752c5695ef11fe14434bbff50dfbe8ea3551
Peter Jaszkowiak <p.jaszkow@gmail.com>
2018-05-15 14:42:37 -0600

    Use travis to build binary modules

--- build.js
+++ build.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const { join } = require('path');
-const { copyFileSync } = require('fs');
+const fs = require('fs');
 
 const source = join(__dirname, 'native/index.node');
 const dest = join(__dirname, `pre-built/${process.platform}_${process.versions.modules}.node`);
+
+const copyFileSync = fs.copyFileSync || ((source, dest) => fs.writeFileSync(dest, fs.readFileSync(source)));
 
 copyFileSync(source, dest);
 process.stdout.write('\n\n\x1b[32mSuccessfully built native benchpress-rs addon.\n' +
